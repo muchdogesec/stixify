c5cc6e90d70bea860082f98959a9830a77ff6b21
David Greenwood <himynamesdave@users.noreply.github.com>
2024-11-13 07:11:49 +0000

    updating tests (#79)

--- tests/add_files.py
+++ tests/add_files.py
@@ -9,7 +9,7 @@ JOBS_ENDPOINT = f"{BASE_URL}jobs/"
 REPORTS_ENDPOINT = f"{BASE_URL}reports/"
 
 # Directory path for files
-FILES_DIR = os.path.join(os.path.dirname(__file__), "files/pdf")
+FILES_DIR = os.path.join(os.path.dirname(__file__), "files")
 
 # Function to delete a report by its ID
 def delete_report(report_id):
@@ -25,7 +25,7 @@ def delete_report(report_id):
         print(f"Failed to delete report {report_id}: {response.status_code} - {response.text}")
 
 # Helper function to make the file upload request
-def upload_file(profile_id, mode, name, tlp_level, confidence, labels, identity, file_path, report_id):
+def upload_file(profile_id, mode, name, tlp_level, confidence, labels, file_path, report_id):
     with open(file_path, 'rb') as file:
         files = {
             'profile_id': (None, profile_id),
@@ -34,7 +34,6 @@ def upload_file(profile_id, mode, name, tlp_level, confidence, labels, identity,
             'tlp_level': (None, tlp_level),
             'confidence': (None, str(confidence)),
             'labels': (None, ','.join(labels)),
-            'identity': (None, identity),
             'file': (os.path.basename(file_path), file, 'application/pdf'),
             'report_id': (None, report_id),
         }
@@ -78,7 +77,6 @@ def run_single_test(test_case):
         tlp_level=test_case["tlp_level"],
         confidence=test_case["confidence"],
         labels=test_case["labels"],
-        identity=test_case["identity"],
         file_path=file_path,
         report_id=test_case["report_id"]
     )
@@ -92,6 +90,7 @@ def run_single_test(test_case):
 def run_all_tests():
     # Define test cases as a list of dictionaries
     test_cases = [
+    # PDF
         {
             "profile_id": "2919ca71-e60c-5aad-81f7-8cf561645d03",
             "mode": "pdf",
@@ -99,22 +98,31 @@ def run_all_tests():
             "tlp_level": "clear",
             "confidence": 99,
             "labels": ["label1", "label2"],
-            "identity": '{"type":"identity","spec_version":"2.1","id":"identity--9779a2db-f98c-5f4b-8d08-8ee04e02dbb5","name":"Dummy Identity"}',
-            "file_name": "fanged_data_good.pdf",
+            "file_name": "pdf/fanged_data_good.pdf",
             "report_id": "6cb8665e-3607-4bbe-a9a3-c2a46bd13630"
         },
         {
             "profile_id": "3919da72-e60c-5aad-82f7-8cf561645d03",
             "mode": "pdf",
-            "name": "Sample Report 2",
+            "name": "txt2stix all cases pdf",
             "tlp_level": "amber",
             "confidence": 80,
-            "labels": ["example_label"],
-            "identity": '{"type":"identity","spec_version":"2.1","id":"identity--a123456b-f98c-5f4b-8d08-8ee04e02dbb5","name":"Another Identity"}',
-            "file_name": "txt2stix-all-cases.pdf",
+            "labels": ["label1"],
+            "file_name": "pdf/txt2stix-all-cases.pdf",
             "report_id": "b2869cb5-5270-4543-ac71-601cc8cd2e3b"
         },
-        # Add more test cases as needed
+    # HTML article
+        {
+            "profile_id": "3919da72-e60c-5aad-82f7-8cf561645d03",
+            "mode": "html_article",
+            "name": "GroupIB 0ktapus",
+            "tlp_level": "amber+strict",
+            "confidence": 80,
+            "labels": ["label1"],
+            "file_name": "html-real/group-ib-0ktapus.html",
+            "report_id": "5795e067-72a4-4953-87ed-f6c56dc6f639"
+        }
+
     ]
 
     # Step 1: Delete any existing reports to avoid conflicts
