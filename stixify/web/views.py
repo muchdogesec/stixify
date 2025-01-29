28e35b9f7995994417d96b18c768f95208dda536
Chaos Pjeles <chaos@efqr.dev>
2025-01-29 11:50:49 +0100

    add objects/delete (#105)
    * add objects/delete
    
    * adding new release of dogesec commons
    
    * Update requirements.txt
    
    ---------
    
    Co-authored-by: David G <himynamesdave@users.noreply.github.com>

--- stixify/web/views.py
+++ stixify/web/views.py
@@ -25,6 +25,7 @@ import django_filters.rest_framework as filters
 from stixify.worker.tasks import new_task
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
+## markdown helper
 import textwrap
 import mistune, hyperlink
 from mistune.renderers.markdown import MarkdownRenderer
@@ -46,28 +47,8 @@ class MarkdownImageReplacer(MarkdownRenderer):
     def codespan(self, token: dict[str, dict], state: mistune.BlockState) -> str:
         token['raw'] = unescape(token['raw'])
         return super().codespan(token, state)
+## markdown helper ends
 
-import textwrap
-import mistune, hyperlink
-from mistune.renderers.markdown import MarkdownRenderer
-from mistune.util import unescape
-class MarkdownImageReplacer(MarkdownRenderer):
-    def __init__(self, request, queryset):
-        self.request = request
-        self.queryset = queryset
-        super().__init__()
-    def image(self, token: dict[str, dict], state: mistune.BlockState) -> str:
-        src = token['attrs']['url']
-        if not hyperlink.parse(src).absolute:
-            try:
-                token['attrs']['url'] = self.request.build_absolute_uri(self.queryset.get(name=src).file.url)
-            except Exception as e:
-                pass
-        return super().image(token, state)
-    
-    def codespan(self, token: dict[str, dict], state: mistune.BlockState) -> str:
-        token['raw'] = unescape(token['raw'])
-        return super().codespan(token, state)
 # Create your views here.
 @extend_schema_view(
     list=extend_schema(
