110b007472e5a8f36f451c3107e7d8c0a3badb1e
Chaos Pjeles <chaos@efqr.dev>
2025-04-29 13:41:06 +0100

    Add retrieve identity (#176)
    * skip system identities in list #175
    
    * add retrieve identity endpoint #173
    
    * Update views.py
    
    ---------
    
    Co-authored-by: David Greenwood <himynamesdave@users.noreply.github.com>

--- stixify/web/views.py
+++ stixify/web/views.py
@@ -563,6 +563,11 @@ class IdentityView(viewsets.ViewSet):
         "name_descending",
         "name_ascending",
     ]
+    SYSTEM_IDENTITIES = [
+        "identity--72e906ce-ca1b-5d73-adcd-9ea9eb66a1b4",
+        "identity--9779a2db-f98c-5f4b-8d08-8ee04e02dbb5",
+        "identity--f92e15d9-6afc-5ae2-bb3e-85a1fd83a3b5"
+    ]
     openapi_tags = ["Identities"]
     skip_list_view = True
     lookup_url_kwarg = "identity_id"
@@ -616,15 +621,18 @@ class IdentityView(viewsets.ViewSet):
         helper = ArangoDBHelper(settings.VIEW_NAME, self.request)
         binds = {
             "@view": settings.VIEW_NAME,
+            "system_identities": self.SYSTEM_IDENTITIES,
         }
         more_filters = []
         if name := helper.query.get('name'):
             binds['name'] = "%" + name.replace('%', r'\%') + "%"
             more_filters.append('FILTER doc.name LIKE @name')
 
+        more_filters.append("FILTER doc.id NOT IN @system_identities")
+
         query = """
         FOR doc IN @@view
-        SEARCH doc.type == "identity"
+        SEARCH doc.type == "identity" AND doc._is_latest == TRUE
         #more_filters
         #sort_stmt
         LIMIT @offset, @count
@@ -636,4 +644,19 @@ class IdentityView(viewsets.ViewSet):
                 self.SORT_PROPERTIES
             )
         ).replace('#more_filters', '\n'.join(more_filters))
+        return helper.execute_query(query, bind_vars=binds)
+    
+    def retrieve(self, request, *args, identity_id=None, **kwargs):
+        helper = ArangoDBHelper(settings.VIEW_NAME, self.request)
+        binds = {
+            "@view": settings.VIEW_NAME,
+            "identity_id": identity_id,
+        }
+        query = """
+        FOR doc IN @@view
+        SEARCH doc.type == "identity" AND doc._is_latest == TRUE AND doc.id == @identity_id
+        COLLECT id = doc.id INTO docs LET doc = docs[0].doc
+        LIMIT @offset, @count
+        RETURN KEEP(doc, KEYS(doc, TRUE))
+        """
         return helper.execute_query(query, bind_vars=binds)
