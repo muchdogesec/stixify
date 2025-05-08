ada3524d21c50be83d5eb172f3dabc783d25499a
Fadl <chaos@efqr.dev>
2025-05-08 16:08:15 +0100

    Add `ignore_embedded_sro` to reports/objects #189

--- stixify/web/views.py
+++ stixify/web/views.py
@@ -395,6 +395,7 @@ class ReportView(viewsets.ViewSet):
                 description="Filter the results by one or more STIX Object types",
                 enum=OBJECT_TYPES,
             ),
+            OpenApiParameter('ignore_embedded_sro', type=bool, description="If set to `true` all embedded SROs are removed from the response."),
             
         ],
     )
@@ -536,6 +537,7 @@ class ReportView(viewsets.ViewSet):
         helper = ArangoDBHelper(settings.VIEW_NAME, self.request)
 
         types = helper.query.get('types', "")
+        filters = []
         bind_vars = {
                 "@collection": settings.VIEW_NAME,
                 'report_id': report_id,
@@ -546,6 +548,9 @@ class ReportView(viewsets.ViewSet):
             bind_vars['visible_to'] = q
             bind_vars['marking_visible_to_all'] = TLP_LEVEL_STIX_ID_MAPPING[TLP_Levels.GREEN], TLP_LEVEL_STIX_ID_MAPPING[TLP_Levels.CLEAR]
             visible_to_filter = 'FILTER doc.created_by_ref == @visible_to OR @marking_visible_to_all ANY IN doc.object_marking_refs'
+        if q := helper.query_as_bool('ignore_embedded_sro', default=False):
+            filters.append('FILTER doc._is_ref != TRUE')
+
         query = """
             LET report = FIRST(
                 FOR doc in @@collection
@@ -556,9 +561,10 @@ class ReportView(viewsets.ViewSet):
             FOR doc in @@collection
             SEARCH report != NULL AND doc._stixify_report_id == @report_id
             FILTER NOT @types OR doc.type IN @types
+            #more_filters
             LIMIT @offset, @count
             RETURN KEEP(doc, KEYS(doc, TRUE))
-        """.replace('#visible_to', visible_to_filter)
+        """.replace('#visible_to', visible_to_filter).replace('#more_filters', '\n'.join(filters))
         return helper.execute_query(query, bind_vars=bind_vars)
 
 @extend_schema_view(
