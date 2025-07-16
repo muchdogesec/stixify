2cced6aa01d578a9e9960a2a9e540c5358fc3a26
David G <himynamesdave@users.noreply.github.com>
2025-07-16 06:47:36 +0100

    Update views.py

--- stixify/web/views.py
+++ stixify/web/views.py
@@ -681,7 +681,7 @@ class IdentityView(viewsets.ViewSet):
 @extend_schema(
     responses={204:{}},
     tags=["Server Status"],
-    summary="Check if service is running",
+    summary="Check if the service is running",
     description=textwrap.dedent(
         """
         If this endpoint returns a 204, the service is running as expected.
