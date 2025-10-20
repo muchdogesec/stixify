f3ee44a966f7f7a1fb50a0864934b2e033af8b54
Chaos Pjeles <chaos@efqr.dev>
2025-10-20 14:36:33 +0100

    Fix completion time (#274)
    * fix issue wsith `job.completion_time` #273
    
    * rename `stixify -> stixify_source` in external_references #266

--- stixify/worker/tasks.py
+++ stixify/worker/tasks.py
@@ -1,3 +1,4 @@
+from datetime import UTC, datetime
 import logging
 import os
 from pathlib import Path
@@ -13,13 +14,15 @@ from django.core.files.base import File as DjangoFile
 import stix2
 
 from stixify.worker import pdf_converter
+
 POLL_INTERVAL = 1
 
 
 def new_task(job: Job):
-    ( process_post.s(job.id) | job_completed_with_error.si(job.id)).apply_async(
+    (process_post.s(job.id) | job_completed_with_error.si(job.id)).apply_async(
         countdown=POLL_INTERVAL, root_id=str(job.id), task_id=str(job.id)
     )
+
 
 @shared_task
 def process_post(job_id, *args):
@@ -28,7 +31,13 @@ def process_post(job_id, *args):
     try:
         job.state = models.JobState.PROCESSING
         job.save()
-        processor = StixifyProcessor(default_storage.open(file.file.name), job.profile, job_id=job.id, file2txt_mode=file.mode, report_id=file.id)
+        processor = StixifyProcessor(
+            default_storage.open(file.file.name),
+            job.profile,
+            job_id=job.id,
+            file2txt_mode=file.mode,
+            report_id=file.id,
+        )
         external_refs = [
             dict(
                 source_name="stixify_profile_id",
@@ -36,8 +45,8 @@ def process_post(job_id, *args):
             )
         ]
         for source in file.sources or []:
-            source_ref = dict(source_name='stixify')
-            if source.startswith('http://') or source.startswith('https://'):
+            source_ref = dict(source_name="stixify_source")
+            if source.startswith("http://") or source.startswith("https://"):
                 source_ref.update(url=source)
             else:
                 source_ref.update(description=source)
@@ -50,29 +59,35 @@ def process_post(job_id, *args):
             confidence=file.confidence,
             labels=file.labels,
             created=file.created,
-            kwargs=dict(
-                external_references=external_refs
-            ),
+            kwargs=dict(external_references=external_refs),
         )
-        processor.setup(report_prop=report_props, extra=dict(_stixify_file_id=str(file.id)))
+        processor.setup(
+            report_prop=report_props, extra=dict(_stixify_file_id=str(file.id))
+        )
         processor.process()
         if processor.incident:
             file.ai_describes_incident = processor.incident.describes_incident
             file.ai_incident_summary = processor.incident.explanation
             file.ai_incident_classification = processor.incident.incident_classification
 
-        file.txt2stix_data = processor.txt2stix_data.model_dump(mode="json", exclude_defaults=True, exclude_unset=True, exclude_none=True)
+        file.txt2stix_data = processor.txt2stix_data.model_dump(
+            mode="json", exclude_defaults=True, exclude_unset=True, exclude_none=True
+        )
         file.summary = processor.summary
-        file.markdown_file.save('markdown.md', processor.md_file.open(), save=True)
+        file.markdown_file.save("markdown.md", processor.md_file.open(), save=True)
 
-        models.FileImage.objects.filter(report=file).delete() # remove old references
+        models.FileImage.objects.filter(report=file).delete()  # remove old references
 
         for image in processor.md_images:
-            models.FileImage.objects.create(report=file, file=DjangoFile(image, image.name), name=image.name)
+            models.FileImage.objects.create(
+                report=file, file=DjangoFile(image, image.name), name=image.name
+            )
 
-        converted_file_path = processor.tmpdir/'converted_pdf.pdf'
+        converted_file_path = processor.tmpdir / "converted_pdf.pdf"
         pdf_converter.make_conversion(processor.filename, converted_file_path)
-        file.pdf_file.save(converted_file_path.name, open(converted_file_path, mode='rb'))
+        file.pdf_file.save(
+            converted_file_path.name, open(converted_file_path, mode="rb")
+        )
         file.save()
     except Exception as e:
         job.error = "failed to process report"
@@ -89,4 +104,4 @@ def job_completed_with_error(job_id):
     if job.error:
         state = models.JobState.FAILED
         job.file.delete()
-    Job.objects.filter(pk=job_id).update(state=state)
+    Job.objects.filter(pk=job_id).update(state=state, completion_time=datetime.now(UTC))
