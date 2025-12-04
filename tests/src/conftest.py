d835bc6c41202d56275e038731862e3b7eb4d44f
Fadl <chaos@efqr.dev>
2025-12-04 11:23:54 +0100

    use in memory broker for tests

--- tests/src/conftest.py
+++ tests/src/conftest.py
@@ -6,6 +6,5 @@ def celery_eager():
     from stixify.worker.celery import app
 
     app.conf.task_always_eager = True
-    app.conf.broker_url = None
     yield
     app.conf.task_always_eager = False
