d95090c604893569e1d02f54c90597e0ffa6cd1e
Lullah <chaos@efqr.dev>
2026-02-09 11:36:04 +0100

    connect auto update signal + tests #296

--- stixify/web/identities.py
+++ stixify/web/identities.py
@@ -1,8 +1,105 @@
+import logging
 import textwrap
+import typing
 from dogesec_commons.utils import Pagination
 from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from dogesec_commons.identity.views import IdentityView as BaseIdentityView
+from dogesec_commons.identity.models import Identity
+from dogesec_commons.objects.helpers import ArangoDBHelper
+from django.conf import settings
+
+if typing.TYPE_CHECKING:
+    from .. import settings
+
+from django.db.models.signals import post_delete, post_save
+from django.dispatch import receiver
+from django.utils import timezone
+
+def classify_objects(object_ids: list[str]):
+    collections = {}
+    for doc_id in object_ids:
+        collection, _, key = doc_id.partition("/")
+        collection_holder: list = collections.setdefault(collection, [])
+        collection_holder.append(key)
+    return collections
+
+
+
+def delete_identity_cleanup(identity: Identity):
+    identity_id = identity.id
+    assert isinstance(identity_id, str)
+
+    helper = ArangoDBHelper(settings.VIEW_NAME, None)
+    vertices = helper.execute_query(
+        """
+        FOR doc IN @@view
+        FILTER doc.id == @identity_id OR doc.created_by_ref == @identity_id
+        RETURN doc._id
+    """,
+        bind_vars={"identity_id": identity_id, "@view": settings.VIEW_NAME},
+        paginate=False,
+    )
+
+    objects = helper.execute_query(
+        """
+        FOR doc IN @@view
+        FILTER
+                doc.id == @identity_id OR
+                doc.created_by_ref == @identity_id OR
+                doc._from IN @vertex_ids OR doc._to IN @vertex_ids
+        RETURN doc._id
+    """,
+        bind_vars={
+            "identity_id": identity_id,
+            "@view": settings.VIEW_NAME,
+            "vertex_ids": vertices,
+        },
+        paginate=False,
+    )
+
+    logging.info(f"removing {len(objects)} objects")
+    for collection, documents in classify_objects(objects).items():
+        logging.info(f"removing {len(documents)} documents from {collection}")
+        helper.db.collection(collection).delete_many(
+            [dict(_key=key) for key in documents], silent=True
+        )
+
+def auto_update_identities(instance: Identity):
+    stix_obj = instance.dict
+    stix_obj["_record_modified"] = timezone.now().isoformat().replace("+00:00", "Z")
+    stix_obj['_product_identity'] = True
+    query = """
+    FOR doc IN @@vertex_collection
+    FILTER doc.id == @identity.id
+    UPDATE doc WITH @identity IN @@vertex_collection
+    RETURN doc._key
+    """
+    binds = {
+        "@vertex_collection": 'stixify_vertex_collection',
+        "identity": stix_obj,
+    }
+
+    from django.http.request import HttpRequest
+    from rest_framework.request import Request
+
+    helper = ArangoDBHelper(settings.VIEW_NAME, Request(HttpRequest()))
+    try:
+        updated_keys = helper.execute_query(query, bind_vars=binds, paginate=False)
+        logging.info(f"updated {len(updated_keys)} identities for {instance.id}")
+        return updated_keys
+    except Exception as e:
+        logging.exception("could not update identities")
+
+@receiver(post_save, sender=Identity)
+def auto_update_identities_callback(sender, instance: Identity, created, **kwargs):
+    if not created:
+        auto_update_identities(instance)
+
+@receiver(post_delete, sender=Identity)
+def delete_identity_cleanup_callback(sender, instance: Identity, **kwargs):
+    return delete_identity_cleanup(instance)
+
 
 @extend_schema_view(
     destroy=extend_schema(
