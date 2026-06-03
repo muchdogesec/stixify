b184413e8d8751890b517c0450a09d186cb4151e
lullah <chaos@efqr.dev>
2026-06-03 14:55:24 +0100

    use cache to speed up statistics endpoint

--- tests/src/settings.py
+++ tests/src/settings.py
@@ -1,3 +1,10 @@
 from stixify.settings import *
 
 ARANGODB_DATABASE = 'stixify_test'
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-cache-name",
+    }
+}
+
