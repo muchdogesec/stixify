60fd132e906d269a40c8835316432e4cff273b3c
lullah <chaos@efqr.dev>
2026-07-28 16:13:02 +0100

    add migration file

--- stixify/web/models.py
+++ stixify/web/models.py
@@ -77,7 +77,7 @@ class CommonSTIXProps(models.Model):
 
     labels = ArrayField(base_field=models.CharField(max_length=256), default=list, help_text="These will be added to the `labels` property of the STIX Report object generated")
     identity = models.JSONField(default=default_identity, validators=[validate_identity], help_text="""This is a full STIX Identity JSON. e.g. `{"type":"identity","spec_version":"2.1","id":"identity--b1ae1a15-6f4b-431e-b990-1b9678f35e15","name":"Dummy Identity"}`. If no value is passed, [the Stixify identity object will be used](https://raw.githubusercontent.com/muchdogesec/stix4doge/refs/heads/main/objects/identity/stixify.json).""")
-    created = models.DateTimeField(default=dj_timezone.now, help_text="This will be used as the `created` time of the STIX Report object generated. Defaults to the current time if not passed. This cannot be changed once the File has been created.")
+    created = models.DateTimeField(default=dj_timezone.now)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
